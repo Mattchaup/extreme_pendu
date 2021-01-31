64304bf4c84cc819848c1bb9c52905176b7f7104
Mattchau_nakamura <67558588+Mattchaup@users.noreply.github.com>
2021-01-31 21:53:31 +0100

    Add files via upload

--- modif_dico.py
+++ modif_dico.py
@@ -1,3 +1,5 @@
+from random import*
+
 def ouverturDico():
     dico=[]
     
@@ -21,12 +23,27 @@ def chercher_lettre_chelou(dico):
                     lettre_chelou.append(lettre)
     return lettre_chelou
 
+def chercher_lettre_poss(dico,condition):
+    mot_recherché = []
+    for mot in dico:
+        if mot[condition[0]] == condition[1]:
+            mot_recherché.append(mot)
+    return mot_recherché
+
+def premier_tri(dico,nbr):
+    liste = []
+    for mot in dico:
+        if len(mot)== nbr:
+            liste.append(mot)
+    return liste
+
 def chercher_lettre(dico,lettre_recherchée):
     mot_recherché = []
     for mot in dico:
         for lettre in mot:
             if lettre == lettre_recherchée:
-                mot_recherché.append(mot)
+                if mot not in mot_recherché:
+                    mot_recherché.append(mot)
     return mot_recherché
 
 def modifier_lettre(dico,lettres_a_modif,nouv_lettre):
@@ -37,9 +54,55 @@ def modifier_lettre(dico,lettres_a_modif,nouv_lettre):
     print(dico[1])
     return dico
 
+def jeuDuConnais(dico):
+    trouver= ""
+    coup = 0
+    oui = 0
+
+    while trouver != "stop":
+        n = randint(0,len(dico))
+        coup += 1
+        trouver = input(f"Connaissez vous le mot {dico[n]} : ")
+        if trouver == "o":
+            oui += 1
+        print(f"Votre ratio est de : {round(oui/coup,3)}")
+
+def plusGrandMot(dico):
+    max = 0
+    for mot in dico:
+        if len(mot) > max:
+            max = len(mot)
+    return max
+
+def correctWord(mot):
+    for lettre in mot:
+        if lettre == " " or lettre == "'" or lettre == "." or lettre == "-":
+            return False
+    return True
+
 dico = ouverturDico()
+print(plusGrandMot(dico))
+#poss = premier_tri(dico,8)
+
+"""
+condition = [[0,'a'],[2,'p'],[6,'r']]
+for cond in condition:
+    poss = chercher_lettre_poss(poss,cond)
+print(poss)
+"""
+
+#poss = premier_tri(dico,5)
+#poss = chercher_lettre_poss(poss,[3,'o'])
 #print(dico.index('cheval'))
 #print(chercher_lettre_chelou(dico))
-#print(chercher_lettre(dico,'cheval'))
+
+"""#Supprime tout les mots avec des lettres_chelou
+liste2 = []
+for mot in dico:
+    if correctWord(mot):
+        liste2.append(mot)
+"""
+
+#print(chercher_lettre(dico,"."))
 #dico = modifier_lettre(dico,[')','!'],'')
-#réecrire_dico(dico)
+#réecrire_dico(liste2)
--- pendu_extrême.py
+++ pendu_extrême.py
@@ -1,10 +1,9 @@
-import random
+import time
 
 def ouverturDico():
     dico=[]
-
     with open ("dico_fr.txt", 'r') as doss:
-        lmots=doss.readlines()
+        lmots = doss.readlines()
     for mot in lmots:
         dico.append(mot[0:-1])
     return dico
@@ -14,6 +13,24 @@ def verifEntier(nbr):
         if x not in ['0','1','2','3','4','5','6','7','8','9']:
             return 0
     return int(nbr)
+
+def verifLetter(lettre,lettre_trouvee,lettre_essayee):
+    if len(lettre) == 1:
+        if lettre in ['a','z','e','r','t','y','u','i','o','p','q','s','d','f','g','h','j','k','l','m','w','x','c','v','b','n']:
+            if lettre not in lettre_trouvee:
+                if lettre in lettre_essayee:
+                    return True
+                else:
+                    print("Tu as déjà essayé cette lettre")
+                    return False
+            else:
+                print("Vous avez déjà trouvé cette lettre")
+                return False
+        else:
+            print("\nChoisissez une lettre qui se trouve dans l'alphabet")
+            return False
+    print("\nVous devez choisir seulement une lettre")
+    return False
 
 def premier_tri(dico,nbr):
     liste = []
@@ -29,57 +46,139 @@ def trouver_lettre(liste,lettre):
             possible.append(mot)
     return possible
 
-def lettre_commun(liste):
-    lettre_communes = [lettre for lettre in liste[0]]
-    for mot in liste:
+def lettre_commun(liste,lettre_trouvee):
+    lettre_communes = []
+    for lettre in liste[0]:
+        if lettre not in lettre_communes and lettre not in lettre_trouvee:
+            lettre_communes.append(lettre)
+
+    for mot in liste[1:]:
         for lettre in lettre_communes:
             if lettre not in mot:
                 lettre_communes.remove(lettre)
-    if len(lettre_communes) > 0:
-        return False
-    else:
-        return True
+    return lettre_communes
 
-def minim_lettre(liste,lettre):
+def EverySame(ressemblance):
+    for i in ressemblance:
+        if i != 1:
+            return False
+    return True
+
+def mot_a_choisir(liste,lettre):
     val = []
+    liste2 = []
     for mot in liste:
-        valMot = 0
-        for x in mot:
-            if x == lettre:
-                valMot += 1
-        val.append(valMot)
-    print(val)
+        val.append(mot.count(lettre))
 
+    minim = min(val)
 
-def afficher_mot(possible,nbr):
-    mot = ''
-    for x in range (nbr):
-        mot += '_ '
+    for mot in liste:
+        if mot.count(lettre) == minim:
+            liste2.append(mot)
+    return liste2
+
+def emplacementCommun(liste,lettre,lettre_trouvee):
+    emplacement = [[] for x in range(len(liste))]
+    liste2 = []
+    ressemblance = [0 for x in range(len(liste))]
+    for i,mot in enumerate(liste):
+        for index,l in enumerate(mot):
+            if l == lettre:
+                emplacement[i].append(index)
+
+    #print("emplacement",emplacement)
+    for x in range(len(emplacement)):
+        for ind in emplacement:
+            if emplacement[x] == ind:
+                ressemblance[x] += 1
+
+    #print("ressemblance",ressemblance)
+    if EverySame(ressemblance):
+        liste2.append(liste[0])
+        for p in range(len(emplacement[0])):
+            g = emplacement[0][p]
+            lettre_trouvee[g] = lettre
+    else:
+        fait = False
+        maxi = max(ressemblance)
+        for i in range(len(liste)):
+            if ressemblance[i] == maxi:
+                if not fait:
+                    for p in range(len(emplacement[i])):
+                        g = emplacement[i][p]
+                        lettre_trouvee[g] = lettre
+                    fait = True
+                liste2.append(liste[i])
+    return liste2, lettre_trouvee
+
+def afficher_lettre_essayee(lettre_essayee):
+    mot = "Vous n'avez pas essayé les lettres : "
+    for i in lettre_essayee:
+        mot += i+", "
     return mot
 
-#Main
+def afficher_mot(lettre_trouvee):
+    mot = ''
+    for x in lettre_trouvee:
+        mot += x + " "
+    return mot
+
+def verifVic(lettre_trouvee):
+    for l in lettre_trouvee:
+        if l == "_":
+            return False
+    return True
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~ Main ~~~~~~~~~~~~~~~~~~~~~~~~~~#
 isInt = False
 findWord = False
+lettre_commune = []
+lettre_essayee = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+coups = 0
 dico = ouverturDico()
 
 while not isInt:
     nbr = input("\nCombien de lettres voulez vous dans votre mot ? : ")
     nbr = verifEntier(nbr)
-    if nbr < 2 or nbr > 24:
-        print("Choisissez un nombre compris entre 2 et 24")
+    if nbr < 2 or nbr > 20:
+        print("Choisissez un nombre compris entre 2 et 20")
     else:
         isInt = True
+    
+lettre_trouvee = ["_" for x in range(nbr)]
+possible = premier_tri(dico,nbr) #liste de mot à nbr lettres
+lettre_commune = lettre_commun(possible,lettre_trouvee)
 
-possible = premier_tri(dico,nbr)
+print("Laisser moi choisir un mot assez difficile à trouver ...")
+time.sleep(2)
+print("C'est bon, j'ai choisis mon mot ;)")
+
 while not findWord:
-    if lettre_commun(possible):
-        lettre = input("Tappez une lettre : ")
-    else:
-        print(possible)
-        word = random.choice(possible)
-        findWord = True
-    possible = trouver_lettre(possible,lettre)
-    print(len(possible))
-    print(afficher_mot(possible,nbr))
+    print(afficher_lettre_essayee(lettre_essayee))
+    print(afficher_mot(lettre_trouvee))
 
-print(word)
+    isCorrect = False
+    while not isCorrect:
+        lettre = input("Choisissez une lettre de l'alphabet : ")
+        isCorrect = verifLetter(lettre,lettre_trouvee,lettre_essayee)
+
+    if lettre in lettre_commune:
+        possible = mot_a_choisir(possible,lettre)
+        possible, lettre_trouvee = emplacementCommun(possible,lettre,lettre_trouvee)
+    
+    else:
+        possible = trouver_lettre(possible,lettre)
+    
+    """
+    if len(possible) <= 20:
+        print(possible)
+    """
+    
+    coups += 1
+    lettre_essayee.remove(lettre)
+    lettre_commune = lettre_commun(possible,lettre_trouvee)
+    findWord = verifVic(lettre_trouvee)
+
+print(afficher_mot(lettre_trouvee))
+print(f"\nBravo vous avez trouvé mon mot en {coups} coups, c'est un peu nul")
+input()
